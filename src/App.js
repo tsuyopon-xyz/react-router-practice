ce8ca721442b768a9e7bd54d587e723bed32ce10
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2019-11-03 10:54:43 +0900

    Routing with Browser Router.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+      </Router>
     </div>
   );
 }
